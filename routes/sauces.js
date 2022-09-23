f93bc6ee0f69098fee7bba0c61fbb468cfe3d405
Quentin Vannelle <quentin.vannelle@hotmail.fr>
2022-09-23 18:25:43 +0200

    mise à jour routes sauces

--- routes/sauces.js
+++ routes/sauces.js
@@ -8,7 +8,7 @@ const saucesCtrl = require('../controllers/sauces');
 
 router.get('/', auth, saucesCtrl.getAllSauces);
 router.post('/', auth, multer, saucesCtrl.createSauce);
-router.get('/:id', auth, saucesCtrl.getOneThing);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 
